c9d0e39f8b31f28819801e7b082a9030c94b5b8b
Juanelo <jdelcidr2@miumg.edu.gt>
2025-12-16 18:08:36 -0600

    segunda fase

--- ResulocionProblemaPalindromo/Program.cs
+++ ResulocionProblemaPalindromo/Program.cs
@@ -1,39 +1,42 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using System.Security.Cryptography.X509Certificates;
+﻿using System;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("introduce la palabra a verificar ");
+        Console.WriteLine("Introduce la palabra a verificar: ");
         string texto = Console.ReadLine();
 
         bool resultado = esPalindromo(texto);
 
-        //Console.WriteLine($" ¡Es palindromo ? {resultado}");
-
-        if (resultado = true)
+        if (resultado == true)  
         {
-            Console.WriteLine($"¡La palabra es un palíndromo! {resultado}");
-
+            Console.WriteLine("¡La palabra es un palíndromo!");
         }
         else
         {
-            Console.WriteLine($"¡La palabra no es un palíndromo! {resultado}");
+            Console.WriteLine("¡La palabra no es un palíndromo!");
         }
-     }
+    }
+
     public static bool esPalindromo(string texto)
     {
-
-        string textoLimpio = texto.ToLower().Replace(" ", "").Replace(".", "").Replace(",", "").Replace("!", "").Replace("?", "").Replace("á", "a").Replace("é", "e").Replace("í", "i").Replace("ó", "o").Replace("ú", "u");
-
+        string textoLimpio = texto.ToLower()
+            .Replace(" ", "")
+            .Replace(".", "")
+            .Replace(",", "")
+            .Replace("!", "")
+            .Replace("?", "")
+            .Replace("á", "a")
+            .Replace("é", "e")
+            .Replace("í", "i")
+            .Replace("ó", "o")
+            .Replace("ú", "u");
 
         char[] caracteres = textoLimpio.ToCharArray();
         Array.Reverse(caracteres);
         string textoReverso = new string(caracteres);
 
         return textoLimpio == textoReverso;
-
-
     }
 }
